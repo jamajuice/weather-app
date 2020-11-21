50b0c1f6fab42245ebd84946b3a6c7527313f153
JamaJuice <jasonmattaa@gmail.com>
2020-11-21 08:53:06 -0500

    initial commit
    base styles

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+const {
+    override,
+    fixBabelImports,
+    addLessLoader,
+  } = require("customize-cra");
+  
+  
+  module.exports = override(
+    addLessLoader({
+      javascriptEnabled: true,
+      modifyVars: { "@primary-color": "#1DA57A" }
+    })
+  );
---
+++ src/components/WButton.js
@@ -0,0 +1,4 @@
+const WButton = (props) => {
+    return <button type="button" className="btn">{props.text}</button>;
+}
+export default WButton;
