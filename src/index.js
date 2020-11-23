150800ee134012b01bcb5346af0452ea969f1610
JamaJuice <jasonmattaa@gmail.com>
2020-11-22 22:36:22 -0500

    GitHub Pages Deploy

--- src/index.js
+++ src/index.js
@@ -4,16 +4,10 @@ import './index.css';
 import './assets/fonts/Daikon/Daikon.css';
 import './assets/fonts/Teko/Teko.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
